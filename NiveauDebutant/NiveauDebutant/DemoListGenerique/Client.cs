d0bcbb1fe9c7c75df7c3fc228a22b55d161ce650
SAID WAHID <said@teknologiate.com>
2025-02-07 15:25:58 +0100

    Ajouter Le projet liste

---
+++ NiveauDebutant/NiveauDebutant/DemoListGenerique/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoListGenerique;
+
+public class Client
+{
+
+    public int Id { get; set; }
+    public string? Nom { get; set; }
+    public string? Prenom { get; set; }
+    public string? Email { get; set; }
+    public string? Telephone { get; set; }
+    public string? Adresse { get; set; }
+
+}
--- NiveauDebutant/NiveauDebutant/DemoListGenerique/Program.cs
+++ NiveauDebutant/NiveauDebutant/DemoListGenerique/Program.cs
@@ -1,2 +1,109 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+#region "Collection liste Simple"
+
+#region "Exemple List"
+
+using DemoListGenerique;
+
+var listeInt = new List<int>()
+            {12, 11,14};
+
+listeInt.Add(1);
+listeInt.Add(3);
+listeInt.Insert(0, 4);
+
+listeInt.Clear();
+
+
+List<string> vs = new List<string>()
+            { "Said", "Omar", "Kenza","Ismail", DateTime.Now.ToString()};
+vs.Add("Kaoutar");
+vs.Add("Amine");
+vs.Add("Salim");
+vs.Add(null);
+vs.Add("Sara");
+
+foreach (string item in vs)
+{
+    Console.WriteLine(item);
+}
+
+#endregion
+
+//Ajouter arrayList.
+string[] etudiantM = new string[3] { "Sofian", "Amal", "Ayoub" };
+vs.AddRange(etudiantM);
+vs.Insert(4, "Khalid");
+
+
+//Ajouter Liste
+var etudiantL = new List<string>()
+            { "Hajar", "Souhaib" };
+vs.AddRange(etudiantL);
+
+
+
+
+//Using simple foreach
+foreach (string item in vs.OrderBy(a => a))
+{
+    Console.WriteLine(item);
+}
+
+//Using foreach Linq Method
+vs.ForEach(e => Console.WriteLine(e));
+
+#endregion
+
+
+
+var listeEtudiant = new List<Etudiant>()
+            {
+                new Etudiant(){id= 1, prenom = "Omar", note = 18},
+                new Etudiant(){id= 2, prenom = "AMAL", note = 16},
+                new Etudiant(){id= 3, prenom = "KHALID", note = 17},
+                new Etudiant(){id= 4, prenom = "Ibtissam", note = 14},
+                new Etudiant(){id= 5, prenom = "Said", note = 11},
+
+            };
+//Update by index
+listeEtudiant[0].note = 12;
+
+//Update by object
+var e2 = new Etudiant();
+e2.id = 1;
+listeEtudiant.Contains(e2);
+
+
+//Update by linq
+var etudiantForUpdate = listeEtudiant.Where(a => a.prenom == "Said").FirstOrDefault();
+
+
+var e1 = new Etudiant();
+e1.id = 6;
+e1.prenom = "Saad";
+e1.note = 15;
+
+listeEtudiant.Add(e1);
+//e1.prenom = 
+
+
+//Order by name
+Console.WriteLine("------------------ Order by name :");
+foreach (var item in listeEtudiant.OrderBy(a => a.nom))
+{
+    Console.WriteLine(item.prenom);
+}
+
+
+//Order by point
+Console.WriteLine("------------------ Les notes :");
+foreach (var item in listeEtudiant.OrderByDescending(a => a.note))
+{
+    Console.WriteLine(item.note + item.prenom);
+}
+
+
+
+
+Console.ReadKey();
