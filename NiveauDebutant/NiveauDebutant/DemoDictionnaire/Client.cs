9c8b00c4edd6190d24f75e558fd729317ad31323
SAID WAHID <said@teknologiate.com>
2025-02-07 15:52:03 +0100

    Commit All

--- NiveauDebutant/NiveauDebutant/DemoDictionnaire/Client.cs
+++ NiveauDebutant/NiveauDebutant/DemoDictionnaire/Client.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DemoDictionnaire;
 
-namespace DemoDictionnaire
+internal class Client
 {
-    internal class Client
-    {
-        public int Id { get; set; }
-        public string Nom { get; set; }
-    }
+    public int Id { get; set; }
+    public string Nom { get; set; } = string.Empty;
 }
